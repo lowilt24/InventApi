33e137434a7f881fe6c80feb054337a34f380b5d
lowilt24 <84353110+lowilt24@users.noreply.github.com.>
2026-06-25 17:35:45 -0500

    modelo Producto y endpoints POST/GET de inventario

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Producto(BaseModel):
+    nombre: str
+    cantidad: int
+    precio: float
+
+inventario: list[Producto] = []
 
 
 @app.get("/")
 def read_root():
     return {"message": "InventApi funcionando"}
+
+
+@app.post("/productos")
+def crear_producto(producto: Producto):
+    inventario.append(producto)
+    return producto
+
+
+@app.get("/productos")
+def listar_productos():
+    return inventario
